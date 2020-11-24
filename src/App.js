cedf02140198d47793bc6afa01d6cd4578203abe
Alison Manning <alisonlaurenmanning@gmail.com>
2020-11-24 10:59:34 -0600

    added another image

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 
 
+
 function App() {
   return (
     <div className="App">
-      <img src="../public/images/phonestats.png"></img>
+      <img src="../public/images/homeless_bargraph.png"></img>
       
     </div>
   );
