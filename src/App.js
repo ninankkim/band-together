2de8cf6e9afb2b54845677945be8cc69ac02cbe3
Nina Kim <ninakim0504@hotmail.com>
2020-11-24 11:18:12 -0500

    material ui

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
+      <img src="../public/images/phonestats.png"></img>
       
     </div>
   );
---
+++ src/screens/AboutUsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function AboutUsScreen() {
+    return (
+        <div>
+            <h1>Band Together</h1>
+            <h2> band/strongertogether/ </h2>
+            <p>The nonprofit project, Band Together, gathers the help of the community to provide information regarding shelters and donations. </p>
+        </div>
+    )
+}
+
+
+
