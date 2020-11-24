093e42bedf7bcd2384e9ccceeee14b197e13ac1f
Nina Kim <ninakim0504@hotmail.com>
2020-11-24 11:27:09 -0500

    app.js file

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <img src="../public/images/phonestats.png"></img>
       
-=======
->>>>>>> 8bf2e66649592d964d8b1b865bd59482226fb124
     </div>
   );
 }
