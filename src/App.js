b32545ca4ce25748f94288b24c07be702d80dd40
Nina Kim <ninakim0504@hotmail.com>
2020-11-24 15:22:47 -0500

    newpages

--- src/App.js
+++ src/App.js
@@ -2,17 +2,64 @@ import React from 'react';
 import './App.css';
 import AboutUsScreen from './screens/AboutUsScreen';
 import { BrowserRouter, Route} from 'react-router-dom';
-
-
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 function App() {
   return (
-    <BrowserRouter>
-    <div className="App">
-      <Route path="/" component={AboutUsScreen} ></Route>
+    
+          <BrowserRouter>
+        <div className="App">
+        <Drawer
+            // container={container}
+            variant="permanent"
+            open={true}
+            // onClose={handleDrawerToggle}
+            anchor="left"
+
+            
+          >
+            <div>
+      <div />
+      <Divider />
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
     </div>
-    </BrowserRouter>
-  );
-}
+          </Drawer>
+          <Route path="/" component={AboutUsScreen} ></Route>
+        </div>
+        </BrowserRouter>    
+      );
+  }
 
 export default App;
