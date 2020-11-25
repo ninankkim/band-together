fec6a9d22b1a579d40ff7301854116701cc47086
Nina Kim <ninakim0504@hotmail.com>
2020-11-25 10:22:25 -0500

    oops

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import AboutUsScreen from './screens/AboutUsScreen';
-import { BrowserRouter, Route} from 'react-router-dom';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
+import SeekDonationScreen from './screens/SeekDonationScreen';
+import SeekHelpScreen from './screens/SeekHelpScreen';
+import ContributorsScreen from './screens/ContributorsScreen';
+
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
 import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
+
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -16,71 +16,72 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import HomeIcon from '@material-ui/icons/Home';
 import HomeWorkIcon from '@material-ui/icons/HomeWork';
-import MailIcon from '@material-ui/icons/Mail';
 import PeopleIcon from '@material-ui/icons/People';
-import MenuIcon from '@material-ui/icons/Menu';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+
 
 function App() {
   return (
-    
-          <BrowserRouter>
-        <div className="App">
-        <Drawer
-            // container={container}
-            variant="permanent"
-            open={true}
-            // onClose={handleDrawerToggle}
-            anchor="left"
 
-            
-          >
-            <div>
-      <div />
-      <Divider />
-      <List>
-        {['Home'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon><InboxIcon /></ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-        {['Add Shelter'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon><HomeWorkIcon /></ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-        {['Find Shelter'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon><HomeIcon /></ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-        {['Contributors'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon><PeopleIcon /></ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      {/* <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List> */}
-    </div>
-          </Drawer>
-          <Route path="/" component={AboutUsScreen} ></Route>
-        </div>
-        </BrowserRouter>    
-      );
-  }
+    <BrowserRouter>
+      <div className="App">
+
+
+
+        <Drawer
+          variant="permanent"
+          open={true}
+          anchor="left">
+          <List className="nav-bar">
+            {['Home', 'Add Shelter', 'Find Shelter', 'Contributors'].map((text, index) => {
+              if (text === 'Home') {
+                return (
+                  <Link to="/">
+                    <ListItem button key={text}>
+                      <ListItemIcon><InboxIcon /></ListItemIcon>
+                      <ListItemText primary={text} />
+                    </ListItem>
+                  </Link>
+                )
+              } else if (text === 'Add Shelter') {
+                return (
+                  <Link to="/donation">
+                  <ListItem button key={text}>
+                    <ListItemIcon><HomeWorkIcon /></ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItem>
+                  </Link>
+                )
+              } else if (text === 'Find Shelter') {
+                return (
+                  <Link to="/help">
+                  <ListItem button key={text}>
+                    <ListItemIcon><HomeIcon /></ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItem>
+                  </Link>
+                )
+              } else {
+                return (
+                  <Link to="/contributors">
+                    <ListItem button key={text}>
+                      <ListItemIcon><PeopleIcon /></ListItemIcon>
+                      <ListItemText primary={text} />
+                    </ListItem>
+                  </Link>
+                )
+              }
+            })}
+          </List>
+        </Drawer>
+        <Switch>
+          <Route exact path="/" component={AboutUsScreen} ></Route>
+          <Route path="/donation" component={SeekDonationScreen}></Route>
+          <Route path="/help" component={SeekHelpScreen}></Route>
+          <Route path="/contributors" component={ContributorsScreen}></Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
 
 export default App;
--- src/screens/AboutUsScreen.js
+++ src/screens/AboutUsScreen.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
-
 export default function AboutUsScreen() {
     return (
-        <div>
-            <h1>Band Together</h1>
-            <h2> "something catchy, one line, describes us" </h2>
-            <p>The nonprofit project, Band Together, gathers the help of the community to provide information regarding shelters and donations. </p>
-            <img src="/images/Homeless_stats.jpg" alt="Graph" ></img>
-            <img src="/images/homelesspos.png" alt="Graph" ></img>
+        <div className="component-container">
+            <img src="/images/bandtogether2.gif" alt=''/>
+            <div className="image-content">
+            <img className="about-image" src="/images/Homeless_stats.jpg" alt="Graph" />
+            <p className="about-text1"> fjeisjflsjfjselfjseljflsjefljselifjslejflsejfl </p>
+            </div>
+            <div className="image-content">
+            <p className="about-text2">fjiejfslekfjseffiejfslefsefsmnefjeks fefefe fefsfef </p>
+            <img className="about-image" src="/images/homelesspos.png" alt="Graph" />
+            </div>
         </div>
     )
 }
