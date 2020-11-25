d4fcd115823aec691163e693b3c72064f24a651e
Nina Kim <ninakim0504@hotmail.com>
2020-11-25 16:01:02 -0500

    HELP JK

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clsx from 'clsx';
 import './index.css';
 import AboutUsScreen from './screens/AboutUsScreen';
+import AddResourcesScreen from './screens/AddResourcesScreen';
 import FindResourcesScreen from './screens/FindResourcesScreen';
-import SeekHelpScreen from './screens/SeekHelpScreen';
 import ContributorsScreen from './screens/ContributorsScreen';
 
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
@@ -17,21 +18,43 @@ import ListItemText from '@material-ui/core/ListItemText';
 import HomeIcon from '@material-ui/icons/Home';
 import HomeWorkIcon from '@material-ui/icons/HomeWork';
 import PeopleIcon from '@material-ui/icons/People';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
 
-function App() {
-  return (
+            
+  function App() {
 
-    <BrowserRouter>
-      <div className="App">
+  const classes = useStyles();
+  const [sidebar, setSideBar] = useState ({
+    left: false,
+  });
 
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
 
-
-        <Drawer
-          variant="permanent"
-          open={true}
-          anchor="left">
-          <List className="nav-bar">
+    setSideBar({ ...sidebar, [anchor]: open });
+  };
+  const list = (anchor) => (
+  <div className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'left',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+    <List className="nav-bar">
             {['Home', 'Add Resources', 'Find Resources', 'Contributors'].map((text, index) => {
               if (text === 'Home') {
                 return (
@@ -44,7 +67,7 @@ function App() {
                 )
               } else if (text === 'Add Resources') {
                 return (
-                  <Link to="/resources">
+                  <Link to="/addresources">
                   <ListItem button key={text}>
                     <ListItemIcon><HomeWorkIcon /></ListItemIcon>
                     <ListItemText primary={text} />
@@ -53,7 +76,7 @@ function App() {
                 )
               } else if (text === 'Find Resources') {
                 return (
-                  <Link to="/shelter">
+                  <Link to="/findresources">
                   <ListItem button key={text}>
                     <ListItemIcon><HomeIcon /></ListItemIcon>
                     <ListItemText primary={text} />
@@ -72,13 +95,32 @@ function App() {
               }
             })}
           </List>
+  </div>
+);
+
+  return (
+
+    <BrowserRouter >
+      <div className="App">
+      {['left'].map((anchor) => (
+
+        <>
+        
+      <Button onClick={toggleDrawer(anchor, true)}><NavigateNextIcon /></Button>
+        <Drawer
+          variant="temporary"
+          anchor={anchor} open={sidebar[anchor]} onClose={toggleDrawer(anchor, false)}
+          >
+          {list(anchor)}
         </Drawer>
         <Switch>
           <Route exact path="/" component={AboutUsScreen} ></Route>
-          <Route path="/resources" component={FindResourcesScreen}></Route>
-          <Route path="/shelter" component={SeekHelpScreen}></Route>
+          <Route path="/addresources" component={AddResourcesScreen}></Route>
+          <Route path="/findresources" component={FindResourcesScreen}></Route>
           <Route path="/contributors" component={ContributorsScreen}></Route>
         </Switch>
+        </>
+        ))}
       </div>
     </BrowserRouter>
   );
