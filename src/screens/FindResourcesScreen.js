3f65f95c7d1bed742bc9a4ef6c80375d3af0642f
Nina Kim <ninakim0504@hotmail.com>
2020-11-30 12:59:48 -0500

    forms

--- src/screens/FindResourcesScreen.js
+++ src/screens/FindResourcesScreen.js
@@ -1,8 +1,23 @@
 import React, { useState } from 'react'
-import { FormControl, FormLabel, FormControlLabel, Radio, RadioGroup } from '@material-ui/core'
+import { FormControl, FormLabel, FormControlLabel, Radio, RadioGroup, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import NavigationIcon from '@material-ui/icons/Navigation';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+}));
 
 
 export default function AddResourceScreen() {
+    const classes = useStyles();
+    const [name, setName] = useState('')
     const [gender, setGender] = useState('')
     const [child, setChild] = useState('')
     const [ident, setIdent] = useState('')
@@ -20,51 +35,71 @@ export default function AddResourceScreen() {
     const handlePet = (event) => {
         setPet(event.target.value);
     }
+    const handleName = (event) => {
+        setName(event.target.value);
+    }
 
 
     return (
         <div>
             <div>
-                <h1>Already Know the name of the Shelter?</h1>
-                
+
             </div>
             <img src="images/shelter.png" width="1800px" border="5px solid black" alt="find shelter heading"></img>
+            <h1>Already Know the name of the Shelter?</h1>
+            <form className={classes.root} noValidate autoComplete="off">
+                <TextField id="filled-basic" variant="filled" placeholder="Alison's Digs" onChange={handleName} />
+                <Fab variant="extended">
+                    <AddIcon className={classes.extendedIcon} />
+        Submit
+        </Fab>
+            </form>
+
+
+
             <h1>Let us know more about you so we can help you find the right shelter</h1>
-            <FormControl className="formfield" component="fieldset">
-                <FormLabel component="legend">Gender (can leave blank)</FormLabel>
-                <RadioGroup aria-label="gender" name="gender" onChange={handleGender}>
-                    <FormControlLabel value="female" control={<Radio />} label="Female" />
-                    <FormControlLabel value="male" control={<Radio />} label="Male" />
-                </RadioGroup>
-            </FormControl>
-            <p></p>
-            <FormControl className="formfield" component="fieldset">
-                <FormLabel component="legend">Bringing Your Child?</FormLabel>
-                <RadioGroup aria-label="gender" name="child"  onChange={handleChild}>
-                    <FormControlLabel value="yesChild" control={<Radio />} label="Yes" />
-                    <FormControlLabel value="noChild" control={<Radio />} label="No" />
-                </RadioGroup>
-            </FormControl>
-            <p></p>
-            <FormControl className="formfield" component="fieldset">
-                <FormLabel component="legend">LGBTQ+ Friendly?</FormLabel>
-                <RadioGroup aria-label="gender" name="lgbtq" onChange={handleIdent}>
-                    <FormControlLabel value="LGBTQFriendly" control={<Radio />} label="Yes" />
-                    <FormControlLabel value="notfriendly" control={<Radio />} label="No" />
-                </RadioGroup>
-            </FormControl>
-            <p></p>
-            <FormControl className="formfield" component="fieldset">
-                <FormLabel component="legend">Pet Shelter Friendly?</FormLabel>
-                <RadioGroup aria-label="gender" name="pet" onChange={handlePet}>
-                    <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-                    <FormControlLabel value="No" control={<Radio />} label="No" />
-                </RadioGroup>
-            </FormControl>
-            <h2>Find Your Shelter</h2>
-            <h5>map api rendering here....</h5>
-            <button>Find Shelter</button>
-            </div>
+
+
+            <form>
+                <FormControl className="formfield" component="fieldset">
+                    <FormLabel component="legend">Gender (can leave blank)</FormLabel>
+                    <RadioGroup aria-label="gender" name="gender" onChange={handleGender}>
+                        <FormControlLabel value="female" control={<Radio />} label="Female" />
+                        <FormControlLabel value="male" control={<Radio />} label="Male" />
+                    </RadioGroup>
+                </FormControl>
+                <p></p>
+                <FormControl className="formfield" component="fieldset">
+                    <FormLabel component="legend">Bringing Your Child?</FormLabel>
+                    <RadioGroup aria-label="gender" name="child" onChange={handleChild}>
+                        <FormControlLabel value="yesChild" control={<Radio />} label="Yes" />
+                        <FormControlLabel value="noChild" control={<Radio />} label="No" />
+                    </RadioGroup>
+                </FormControl>
+                <p></p>
+                <FormControl className="formfield" component="fieldset">
+                    <FormLabel component="legend">LGBTQ+ Friendly?</FormLabel>
+                    <RadioGroup aria-label="gender" name="lgbtq" onChange={handleIdent}>
+                        <FormControlLabel value="LGBTQFriendly" control={<Radio />} label="Yes" />
+                        <FormControlLabel value="notfriendly" control={<Radio />} label="No" />
+                    </RadioGroup>
+                </FormControl>
+                <p></p>
+                <FormControl className="formfield" component="fieldset">
+                    <FormLabel component="legend">Pet Shelter Friendly?</FormLabel>
+                    <RadioGroup aria-label="gender" name="pet" onChange={handlePet}>
+                        <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+                        <FormControlLabel value="No" control={<Radio />} label="No" />
+                    </RadioGroup>
+                </FormControl>
+                <p></p>
+                <Fab variant="extended">
+                    <AddIcon className={classes.extendedIcon} />
+        Submit
+        </Fab>
+            </form>
+
+        </div>
 
 
     )
