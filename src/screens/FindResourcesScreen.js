14af7904701f96e3eff2403ed3db2498937990ca
Nina Kim <ninakim0504@hotmail.com>
2020-11-30 14:35:50 -0500

    ehh

--- src/screens/FindResourcesScreen.js
+++ src/screens/FindResourcesScreen.js
@@ -66,43 +66,6 @@ export default function AddResourceScreen() {
 
             <h1>Let us know more about you so we can help you find the right shelter</h1>
 
-            <FormControl className="formfield" component="fieldset">
-                <FormLabel component="legend">Gender (can leave blank)</FormLabel>
-                <RadioGroup aria-label="gender" name="gender" onChange={handleGender}>
-                    <FormControlLabel value="female" control={<Radio />} label="Female" />
-                    <FormControlLabel value="male" control={<Radio />} label="Male" />
-                </RadioGroup>
-            </FormControl>
-            <p></p>
-            <FormControl className="formfield" component="fieldset">
-                <FormLabel component="legend">Bringing Your Child?</FormLabel>
-                <RadioGroup aria-label="gender" name="child"  onChange={handleChild}>
-                    <FormControlLabel value="yesChild" control={<Radio />} label="Yes" />
-                    <FormControlLabel value="noChild" control={<Radio />} label="No" />
-                </RadioGroup>
-            </FormControl>
-            <p></p>
-            <FormControl className="formfield" component="fieldset">
-                <FormLabel component="legend">LGBTQ+ Friendly?</FormLabel>
-                <RadioGroup aria-label="gender" name="lgbtq" onChange={handleIdent}>
-                    <FormControlLabel value="LGBTQFriendly" control={<Radio />} label="Yes" />
-                    <FormControlLabel value="notfriendly" control={<Radio />} label="No" />
-                </RadioGroup>
-            </FormControl>
-            <p></p>
-            <FormControl className="formfield" component="fieldset">
-                <FormLabel component="legend">Pet Shelter Friendly?</FormLabel>
-                <RadioGroup aria-label="gender" name="pet" onChange={handlePet}>
-                    <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-                    <FormControlLabel value="No" control={<Radio />} label="No" />
-                </RadioGroup>
-            </FormControl>
-            <h2>Find Your Shelter</h2>
-            <h5>map api rendering here....</h5>
-            <button>Find Shelter</button>
-            <Map />
-            </div>
-
 
 
             <form>
@@ -144,7 +107,6 @@ export default function AddResourceScreen() {
         </Fab>
             </form>
 
-        </div>
 
 
 
