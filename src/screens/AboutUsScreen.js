bf6d97e67bc850ab3b8c40fd087e471313d3ad36
Alison Manning <alisonlaurenmanning@gmail.com>
2020-12-01 13:23:13 -0600

    minor styling changes

--- src/screens/AboutUsScreen.js
+++ src/screens/AboutUsScreen.js
@@ -7,7 +7,7 @@ export default function AboutUsScreen() {
     
     return (
         <div>
-            <img src="images/heading1.gif" className="responsive"></img>
+            <img src="images/heading.1.gif" className="responsive"></img>
             <img src="images/heading.2.png" className="responsive"></img>
             <img src="images/heading.3.png" className="responsive"></img>
             <img src="images/heading.4.png" className="responsive"></img>
