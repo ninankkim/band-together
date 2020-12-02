49819e19951a076c796a6e0d4bef54b9bdd29856
DasomAnH <ektha116@gmail.com>
2020-12-02 10:44:56 -0500

    map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,24 +1,20 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import ReactMapGL, {Marker} from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import HomeIcon from '@material-ui/icons/Home';
 
-
-
-
 export default function Map() {
-    const [ shelters, setShelters ] = useState([])
-
+  const [shelters, setShelters] = useState([]);
+  const [selectedShelters, setSelectedShelters] = useState(null);
 
   const [viewport, setViewport] = useState({
-    width: "50vw",
-    height: "50vh",
+    width: '50vw',
+    height: '50vh',
     latitude: 38.79182417693575,
     longitude: -97.60391235351562,
-    zoom: 8
+    zoom: 8,
   });
-
 
   // componentDidMount
   useEffect(() => {
@@ -26,26 +22,50 @@ export default function Map() {
       .then(res => res.json())
       .then(data => {
         setShelters(data);
-      })
-  }, [])
-
-
+      });
+  }, []);
 
   return (
     <ReactMapGL
       {...viewport}
-      let mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      mapStyle="mapbox://styles/bargavi-dev/cki67zptc3d5o19try3pjc9fh"
-      onViewportChange={ viewport => {
-          setViewport(viewport);
+      let
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      mapStyle='mapbox://styles/bargavi-dev/cki67zptc3d5o19try3pjc9fh'
+      onViewportChange={viewport => {
+        setViewport(viewport);
       }}
     >
-        {shelters.map((shelter) => {
-          return <Marker key={shelter.id} latitude={shelter.latitude} longitude={shelter.longitude}>
-            <HomeIcon />
+      {shelters.map(shelter => {
+        return (
+          <Marker
+            key={shelter.id}
+            latitude={shelter.latitude}
+            longitude={shelter.longitude}
+            onclick={e => {
+              e.preventDefault();
+              setSelectedShelters(shelters);
+            }}
+          >
+            <HomeIcon></HomeIcon>
           </Marker>
-
-        })}
+        );
+      })}
+      {selectedShelters ? (
+        <Popup
+          latitude={selectedShelters.latitude}
+          longitude={selectedShelters.longitude}
+          onClose={() => {
+            setSelectedShelters(null);
+          }}
+        >
+          <div>
+            <h2>{selectedShelters.properties.name}</h2>
+            <h2>{selectedShelters.properties.phone_number}</h2>
+            <h2>{selectedShelters.properties.lgbtFriendly}</h2>
+            <h2>{selectedShelters.properties.women_and_children}</h2>
+          </div>
+        </Popup>
+      ) : null}
     </ReactMapGL>
   );
 }
