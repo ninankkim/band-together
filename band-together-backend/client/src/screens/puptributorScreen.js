f3e227487d99c0d7232a33d1cc5ed871f88493cf
Alison Manning <alisonlaurenmanning@gmail.com>
2020-12-07 09:19:41 -0600

    contributor/puptributor img

---
+++ band-together-backend/client/src/screens/puptributorScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function puptributorScreen() {
+    return (
+        <div>
+            <img src="images/pupdate.png" className="responsive"></img>
+        </div>
+    )
+}
